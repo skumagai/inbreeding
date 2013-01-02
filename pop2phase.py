2ac2757f788bfc4bf7d9257c1c3c9a2c93ae9307
Seiji Kumagai <seiji.kumagai@gmail.com>
2013-01-02 16:38:42 -0500

    generate csv of all genotypes as an intermediate format before phase file.

--- pop2phase.py
+++ pop2phase.py
@@ -32,39 +32,79 @@ import json
 import sys
 import random
 import os.path
+import operator
 
-# in order to choose between 'long' and 'binary' modules, for
+# From https://github.com/onyxfish/csvkit/commit/5f2c5d9d5c8596a4c7440e53dba0bd8d92feb3b9
+# Ensure SIGPIPE doesn't throw an exception
+# Prevents [Errno 32] Broken pipe errors, e.g. when piping to 'head'
+try:
+    import signal
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+except ImportError:
+    pass
+
+
+### Functions specific to 'csv' subcommand.
+# In order to choose between 'long' and 'binary' modules, for
 # respectively the infinite-alleles and infinite-sites models, command
 # line arguments have to be processed before the import.
 def parseArgs():
     parser = argparse.ArgumentParser(
         description="convert simuPOP's Population object to genotype data format used in phase.")
 
-    parser.add_argument('SAMPLE',
-                        type=int,
-                        help='sample size')
-    parser.add_argument('LOCI',
-                        type=str,
-                        help='number of samples (command separated list)')
-    parser.add_argument('DIR',
-                        nargs='*',
-                        type=str,
-                        help='name of directories storing simuPOP.Population object')
-    parser.add_argument('-r', '--random',
-                             action='store_true',
-                             help='randomize order of samples')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        type=argparse.FileType('w'),
-                        default=sys.stdout,
-                        help='save converted data into FILE')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    csv_p = subparsers.add_parser('csv', help='generate CSV file of genotypes')
+    csv_p.add_argument('DIR',
+                       type=str,
+                       help='directory')
+    csv_p.add_argument('-o', '--output',
+                       metavar='FILE',
+                       type=argparse.FileType('w'),
+                       default=sys.stdout,
+                       help='save converted data into FILE')
+    csv_p.set_defaults(func=do_csv)
+
+
+    phase_p = subparsers.add_parser('phase', help='generate phase file of genotypes')
+    phase_p.add_argument('SAMPLE',
+                         type=int,
+                         help='sample size')
+    phase_p.add_argument('LOCI',
+                         type=str,
+                         help='number of samples (comma separated list)')
+    phase_p.add_argument('CSVS',
+                         type=str,
+                         help='name of CSV files storing genotypes (comma separaeted list)')
+    phase_p.add_argument('-r', '--random',
+                         action='store_true',
+                         help='randomize order of samples')
+    phase_p.add_argument('-o', '--output',
+                         metavar='FILE',
+                         type=argparse.FileType('w'),
+                         default=sys.stdout,
+                         help='save converted data into FILE')
+    phase_p.set_defaults(func=do_phase)
+
     return parser.parse_args()
 
 
 def get_info(dir):
-    with open(os.path.join(dir, 'conf.json'), 'r') as rf:
-        info = json.load(rf)
+    try:
+        with open(os.path.join(dir, 'conf.json'), 'r') as rf:
+            info = json.load(rf)
+    except:
+        print('[ERROR] {} is not a directory.'.format(dir), file=sys.stderr)
+        sys.exit(1)
     return info
+
+
+def get_mode(args):
+    info = get_info(args.DIR)
+
+    mode = 'infinite-sites'
+    if u'mode' in info and info[u'mode'] == u'infinite-alleles':
+        mode = 'infinite-alleles'
+    return mode
 
 
 def encode(locus, codes):
@@ -79,13 +119,11 @@ def encode(locus, codes):
 
 
 def import_right_module(args):
-    mode = 'infinite-sites'
     try:
         global sim
         import simuOpt
-        info = get_info(args.DIR[0])
-        if u'mode' in info and info[u'mode'] == u'infinite-alleles':
-            mode = 'infinite-alleles'
+        mode = get_mode(args)
+        if mode == 'infinite-alleles':
             simuOpt.setOptions(alleleType = 'long')
         else:
             simuOpt.setOptions(alleleType = 'binary')
@@ -95,68 +133,6 @@ def import_right_module(args):
         sys.exit(1)
     return mode
 
-def check_directories_exist(dirs, mode):
-    ds = []
-    for d in dirs:
-        if os.path.isdir(d) is True:
-            with open(os.path.join(d, 'conf.json')) as rf:
-                if json.load(rf)[u'mode'] == mode:
-                    ds.append(d)
-                else:
-                    print('[ERROR] cannot mix mutation models.', file=sys.stderr)
-                    sys.exit(1)
-        else:
-            print('[ERROR] {} is not a directory.'.format(d), file=sys.stderr)
-            sys.exit(1)
-    return ds
-
-
-def convert(args, mode):
-    dirs = check_directories_exist(args.DIR, mode)
-    loci = [int(i) for i in args.LOCI.split(',')]
-    if len(loci) != len(dirs):
-        print('[ERROR] inconsistent sampling strategy.', file=sys.stderr)
-        sys.exit(1)
-
-    inds = [[] for i in xrange(args.SAMPLE)]
-    for d, l in zip(dirs, loci):
-        ind_data = sample_loci(d, l, args.SAMPLE)
-        for i, d in enumerate(ind_data):
-            inds[i].extend(d)
-    print(inds)
-    return inds
-
-def sample_loci(d, nloci, nsample):
-    info = get_info(d)
-    info = get_info(d)
-    pop_size = info[u'population size']
-    num_loci = info[u'number of loci']
-    pop = sim.loadPopulation(os.path.join(d, str(info[u'files'][0])))
-    allele_len = pop.totNumLoci() / num_loci
-
-    ind_idx = random.sample(xrange(pop_size), nsample)
-    inds = [pop.individual(idx) for idx in ind_idx]
-    loci_idx = random.sample(xrange(num_loci), nloci)
-    loci_dict = {i:{'idx': 0} for i in loci_idx}
-
-    genotypes = []
-
-    for ind in inds:
-        geno0 = chunks(ind.genotype(ploidy = 0), allele_len)
-        geno0 = [encode(locus, loci_dict[idx])
-                 for idx, locus in enumerate(geno0)
-                 if idx in loci_idx]
-        geno1 = chunks(ind.genotype(ploidy = 1), allele_len)
-        geno1 = [encode(locus, loci_dict[idx])
-                 for idx, locus in  enumerate(geno1)
-                 if idx in loci_idx]
-
-        genotypes.append((geno0, geno1))
-
-    genotypes = [list(itertools.chain(*[list(i) for i in zip(*ind)]))
-                 for ind in genotypes]
-    return genotypes
-
 
 def chunks(l, n):
     '''Divide a list into equal-length sublists'''
@@ -164,7 +140,82 @@ def chunks(l, n):
         yield l[i:i+n]
 
 
-def write_data(nsample, nloci, data, output):
+def to_csv(args, mode):
+    d = args.DIR
+    info = get_info(d)
+    pop = sim.loadPopulation(os.path.join(d, str(info[u'files'][0])))
+    pop_size = pop.popSize()
+    nloci = info[u'number of loci']
+    lenlocus = pop.totNumLoci() / nloci
+
+    loci_dict = {i:{'idx': 0} for i in xrange(pop_size)}
+    genotypes = []
+
+    for ind in pop.individuals():
+        geno0 = chunks(ind.genotype(ploidy = 0), lenlocus)
+        geno0 = [encode(locus, loci_dict[idx])
+                 for idx, locus in enumerate(geno0)]
+        geno1 = chunks(ind.genotype(ploidy = 1), lenlocus)
+        geno1 = [encode(locus, loci_dict[idx])
+                 for idx, locus in  enumerate(geno1)]
+
+        genotypes.append(itertools.chain.from_iterable(zip(geno0, geno1)))
+    return genotypes
+
+
+def write_csv(genotypes, output):
+    pop_size = len(genotypes)
+    writer = csv.writer(output)
+    writer.writerow([pop_size])
+
+    for genotype in genotypes:
+        writer.writerow(list(genotype))
+
+
+### Functions specific to 'phase' subcommand.
+def get_csvs(args):
+    files = args.CSVS.split(',')
+    for f in files:
+        if os.path.isfile(f) is False:
+            print('[ERROR] {} is not a file.'.format(f), file=sys.stderr)
+            sys.exit(1)
+    return files
+
+
+def to_phase(args):
+    csvs = get_csvs(args)
+    sloci = [int(i) for i in args.LOCI.split(',')]
+    if len(sloci) != len(csvs):
+        print('[ERROR] inconsistent sampling strategy.', file=sys.stderr)
+        sys.exit(1)
+
+    inds = [[] for i in xrange(args.SAMPLE)]
+    for c, l in zip(csvs, sloci):
+        ind_data = sample_loci(c, l, args.SAMPLE)
+        for i, d in enumerate(ind_data):
+            inds[i].extend(d)
+    return inds
+
+
+def sample_loci(c, nloci, nsample):
+    try:
+        with open(c, 'r') as rf:
+            reader = csv.reader(rf)
+            pop_size = int(reader.next()[0])
+            idx = random.sample(xrange(pop_size), nsample)
+            inds = [l for i, l in enumerate(reader) if i in idx]
+    except:
+        print('[ERROR] file cannot be opened: {}.'.format(csv), file=sys.stderr)
+        sys.exit(1)
+
+    totalloci = len(inds[0]) / 2
+    order = [(i * 2, i * 2 + 1) for i in random.sample(xrange(totalloci), nloci)]
+    extractor = operator.itemgetter(*sorted(itertools.chain.from_iterable(order)))
+    loci = [extractor(ind) for ind in inds]
+    return loci
+
+
+def write_phase(nsample, nloci, data, output):
     nloci = sum([int(i) for i in nloci.split(',')])
 
     print(nsample, file=output)
@@ -175,12 +226,36 @@ def write_data(nsample, nloci, data, output):
         print("\t".join([str(di) for di in d]), file=output)
 
 
+def randomize(data):
+    nloci = len(data[0])
+    order = xrange(nloci)
+    random.shuffle(order)
+    randomizer = operator.itemgetter(*order)
+    genotypes = [randomizer(ind) for ind in data]
+    return genotypes
+
+
+### (Almost) top-level functions
+def do_csv(args):
+    '''convert genotypic information in simuPOP.Population object into CSV file.'''
+    mode = import_right_module(args)
+    data = to_csv(args, mode)
+    write_csv(data, args.output)
+    sys.exit(0)
+
+
+def do_phase(args):
+    '''convert genotypic information generated by pop2phase.py csv [...] into phase file.'''
+    data = to_phase(args)
+    if args.random is True:
+        data = randomize(data)
+    write_phase(args.SAMPLE, args.LOCI, data, args.output)
+    sys.exit(0)
+
+
 def main():
     args = parseArgs()
-    mode = import_right_module(args)
-    data = convert(args, mode)
-    write_data(args.SAMPLE, args.LOCI, data, args.output)
-    sys.exit(0)
+    args.func(args)
 
 
 if __name__ == '__main__':
