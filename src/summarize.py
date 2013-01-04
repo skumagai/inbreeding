58e4380035eea5cb5600ca8c89175309bb70f3b9
Seiji Kumagai <Seiji.Kumagai@gmail.com>
2013-01-04 17:29:10 -0500

    add unit tests for summarize.py, and a bit of code clean-up.

--- src/summarize.py
+++ src/summarize.py
@@ -30,8 +30,9 @@ import json
 import csv
 import os.path
 import sys
+import itertools
 
-def parseArgs()
+def parseArgs():
     parser = argparse.ArgumentParser(
         description='compute f, g, P, W')
 
@@ -41,98 +42,66 @@ def parseArgs()
     return parser.parse_args()
 
 
-def import_right_module(args):
-    mode = 'infinite-sites'
-    try:
-        global sim
-        import simuOpt
-        info = get_info(args.DIR[0])
-        if u'mode' in info and info[u'mode'] == u'infinite-alleles':
-            mode = 'infinite-alleles'
-            simuOpt.setOptions(alleleType = 'long')
-        else:
-            simuOpt.setOptions(alleleType = 'binary')
-        import simuPOP as sim
-    except:
-        print('[ERROR] needs simuPOP', file=sys.stderr)
-        sys.exit(1)
-    return mode
+def basic_info(inds):
+    return len(inds), len(inds[0][0])
 
 
-def get_info(dir):
-    with open(os.path.join(dir, 'conf.json'), 'r') as rf:
-        info = json.load(rf)
-    return info
+def add_lists(lst0, lst1):
+    if len(lst0) != len(lst1):
+        raise IndexError('Length mismatch')
+    for i, v in enumerate(lst1):
+        lst0[i] += v
 
 
-def chunks(l, n):
-    '''Divide a list into equal-length sublists'''
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
-
-
-def compute_f(pop, nloci):
-    nsites = pop.totNumLoci() / nloci
+def compute_f(inds):
+    pop_size, nloci = basic_info(inds)
     fs = [0] * nloci
-    pop_size = pop.popSize()
-    for ind in pop.individuals():
-        geno0 = chunks(ind.genotype(ploidy=0), nsites)
-        geno1 = chunks(ind.genotype(ploidy=1), nsites)
-        fs = check_identity(fs, geno0, geno1)
+    for ind in inds:
+        add_lists(fs, check_identity(nloci, ind[0], ind[1]))
     return [float(val) / pop_size for val in fs]
 
 
-def comput_g(pop, nloci):
-    nsites = pop.totNumLoci() / nloci
+def compute_g(inds):
+    pop_size, nloci = basic_info(inds)
     gs = [0] * nloci
-    pop_size = pop.popSize()
-    for pair in itertools.combinations(pop.individuals(), 2):
-        geno00 = chunks(pair[0].genotype(ploidy=0), nsites)
-        geno01 = chunks(pair[0].genotype(ploidy=1), nsites)
-        geno10 = chunks(pair[1].genotype(ploidy=0), nsites)
-        geno11 = chunks(pair[1].genotype(ploidy=1), nsites)
-        gs = check_identity(gs, geno00, geno10)
-        gs = check_identity(gs, geno00, geno11)
-        gs = check_identity(gs, geno01, geno10)
-        gs = check_identity(gs, geno01, geno11)
+    for pair in itertools.combinations(inds, 2):
+        geno00, geno01 = pair[0]
+        geno10, geno11 = pair[1]
+        add_lists(gs, check_identity(nloci, geno00, geno10))
+        add_lists(gs, check_identity(nloci, geno00, geno11))
+        add_lists(gs, check_identity(nloci, geno01, geno10))
+        add_lists(gs, check_identity(nloci, geno01, geno11))
     denom = 4. * pop_size * (pop_size - 1) / 2
     return [float(val) / denom for val in gs]
 
 
-def compute_P(pop, nloci):
-    nsites = pop.totNumLoci() / nloci
+def compute_P(inds):
+    pop_size, nloci = basic_info(inds)
     Ps = {key: 0 for key in itertools.product(xrange(2), repeat = nloci)}
-    zeros = [0] * nloci
-    pop_size = pop.popSize()
-    for ind in pop.individuals():
-        geno0 = chunks(ind.genotype(ploidy=0), nsites)
-        geno1 = chunks(ind.genotype(ploidy=1), nsites)
-        Ps[tuple(check_identity(zeros, geno0, geno1))] += 1
+    for ind in inds:
+        Ps[tuple(check_identity(nloci, ind[0], ind[1]))] += 1
     return {key: float(val) / pop_size for key, val in Ps.iteritems()}
 
 
-def compute_W():
-    nsites = pop.totNumLoci() / nloci
+def compute_W(inds):
+    pop_size, nloci = basic_info(inds)
     Ws = {key: 0 for key in itertools.product(xrange(2), repeat = nloci)}
-    zeros = [0] * nloci
-    pop_size = pop.popSize()
-    for pair in itertools.combinations(pop.individuals(), 2):
-        geno00 = chunks(pair[0].genotype(ploidy=0), nsites)
-        geno01 = chunks(pair[0].genotype(ploidy=1), nsites)
-        geno10 = chunks(pair[1].genotype(ploidy=0), nsites)
-        geno11 = chunks(pair[1].genotype(ploidy=1), nsites)
-        Ws[tuple(check_identity(zeros, geno00, geno10))] += 1
-        Ws[tuple(check_identity(zeros, geno00, geno11))] += 1
-        Ws[tuple(check_identity(zeros, geno01, geno10))] += 1
-        Ws[tuple(check_identity(zeros, geno01, geno11))] += 1
+    for pair in itertools.combinations(inds, 2):
+        geno00, geno01 = pair[0]
+        geno10, geno11 = pair[1]
+        Ws[tuple(check_identity(nloci, geno00, geno10))] += 1
+        Ws[tuple(check_identity(nloci, geno00, geno11))] += 1
+        Ws[tuple(check_identity(nloci, geno01, geno10))] += 1
+        Ws[tuple(check_identity(nloci, geno01, geno11))] += 1
     denom = 4. * pop_size * (pop_size - 1) / 2
     return {key: float(val) / denom for key, val in Ws.iteritems()}
 
 
-def check_identity(counts, geno0, geno1):
+def check_identity(nloci, geno0, geno1):
+    counts = [0] * nloci
     for i, geno_pair in enumerate(zip(geno0, geno1)):
         if geno_pair[0] == geno_pair[1]:
-            counts[i] += 1
+            counts[i] = 1
     return counts
 
 
@@ -140,11 +109,19 @@ def summarise(d, mode):
     info = get_info(d)
     num_loci = info[u'number of loci']
     for f in [str(s) for s in info[u'files']]:
-        pop = sim.loadPopulation(f)
-        f = compute_f(pop, num_loci)
-        g = compute_g(pop, num_loci)
-        P = compute_P(pop, num_loci)
-        W = compute_W(pop, num_loci)
+        pop = sim.loadPopulation(os.path.join(d, f))
+        pop_size = pop.popSize()
+        nsites = pop.totNumLoci() / num_loci
+
+        inds = [[tuple(chunks(ind.genotype(ploidy=0), nsites)),
+                 tuple(chunks(ind.genotype(ploidy=1), nsites))] for ind in pop.individuals()]
+
+        f = compute_f(inds)
+        g = compute_g(inds)
+        P = compute_P(inds)
+        W = compute_W(inds)
+
+        return f, g, P, W
 
 
 def main():
---
+++ src/test/summarize_test.py
@@ -0,0 +1,87 @@
+# -*- mode: python; coding: utf-8; -*-
+
+# summarize_test.py - brief description
+
+# Copyright (C) 2013 Seiji Kumagai
+
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+'''unittest for summarize.py.  Some top-level or nearly-so functions are not tested'''
+
+from __future__ import print_function
+
+import unittest
+import summarize
+
+import mock
+
+class TestFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.inds = [
+            [([0],[1]), ([0],[1])],
+            [([0],[1]), ([1],[0])],
+            [([1],[0]), ([1],[0])]]
+
+
+    def test_basic_info(self):
+        self.assertEqual(summarize.basic_info(self.inds), (3, 2))
+
+
+    def test_add_lists(self):
+        lst0 = [0] * 5
+        lst1 = [10] * 5
+        summarize.add_lists(lst0, lst1)
+        self.assertEqual(lst0, lst1)
+
+        lst2 = [10] * 6
+        self.assertRaises(IndexError, summarize.add_lists, lst0, lst2)
+
+
+    def test_compute_f(self):
+        self.assertEqual(summarize.compute_f(self.inds), [2./3., 2./3.])
+
+
+    def test_compute_g(self):
+        self.assertEqual(summarize.compute_g(self.inds), [1./3., 1./3.])
+
+
+    def test_compute_P(self):
+        exp = {
+            (0,0): 1./3.,
+            (1,1): 2./3.,
+            (0,1): 0,
+            (1,0): 0.
+            }
+        self.assertEqual(summarize.compute_P(self.inds), exp)
+
+
+    def test_compute_W(self):
+        exp = {
+            (0,0): 8./12.,
+            (1,1): 4./12.,
+            (0,1): 0.,
+            (1,0): 0.
+            }
+        self.assertEqual(summarize.compute_W(self.inds), exp)
+
+
+    def test_check_identity(self):
+        self.assertEqual(summarize.check_identity(2, self.inds[0][1], self.inds[1][1]), [0, 0])
