09cb5426369b05b603383e2a7436b9bd16923de8
Seiji Kumagai <Seiji.Kumagai@gmail.com>
2013-01-04 18:27:16 -0500

    generate keys of P and W using a function, as it appears more than one place.

--- src/summarize.py
+++ src/summarize.py
@@ -85,7 +85,7 @@ def compute_P(inds):
 
 def compute_W(inds):
     pop_size, nloci = basic_info(inds)
-    Ws = {key: 0 for key in itertools.product(xrange(2), repeat = nloci)}
+    Ws = {key: 0 for key in generate_PW_keys(nloci)}
     for pair in itertools.combinations(inds, 2):
         geno00, geno01 = pair[0]
         geno10, geno11 = pair[1]
@@ -105,9 +105,14 @@ def check_identity(nloci, geno0, geno1):
     return counts
 
 
+def generate_PW_keys(nloci):
+    return itertools.product(xrange(2), repeat = nloci)
+
+
 def summarise(d, mode):
     info = get_info(d)
     num_loci = info[u'number of loci']
+    keys = list(generate_PW_keys(num_loci))
 
     # this is needed as simuPOP is loaded in a different file.  It's
     # name is not bound in the namescope of this file.
